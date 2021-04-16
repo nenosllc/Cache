1aa840b53dc6b30023a2aa50e7fafc42391740f5
Sam-Spencer <samspencer@hey.com>
2021-04-16 16:17:24 -0500

    Added subscripts

--- Source/Shared/Configuration/MemoryConfig.swift
+++ Source/Shared/Configuration/MemoryConfig.swift
@@ -2,18 +2,32 @@ import Foundation
 
 public struct MemoryConfig {
     
-    /// Expiry date that will be applied by default for every added object
-    /// if it's not overridden in the add(key: object: expiry: completion:) method
+    /// Expiry date that will be applied by default for every added object if it's not
+    /// overridden in the add(key: object: expiry: completion:) method.
+    ///
     public let expiry: Expiry
     
-    /// The maximum number of objects in memory the cache should hold.
-    /// If 0, there is no count limit. The default value is 0.
+    /// The maximum number of objects in memory the cache should hold. If 0, there is no
+    /// count limit. The default value is 0.
+    ///
     public let countLimit: UInt
     
-    /// The maximum total cost that the cache can hold before it starts evicting objects.
-    /// If 0, there is no total cost limit. The default value is 0
+    /// The maximum total cost that the cache can hold before it starts evicting
+    /// objects. If 0, there is no total cost limit. The default value is 0.
+    ///
     public let totalCostLimit: UInt
     
+    /// Create an in-memory cache configuration.
+    ///
+    /// - parameter expiry: Expiry date that will be applied by default for every added
+    ///     object if it's not overridden in the add(key: object: expiry: completion:)
+    ///     method.
+    /// - parameter countLimit: The maximum number of objects in memory the cache should
+    ///     hold. If 0, there is no count limit. The default value is 0.
+    /// - parameter totalCostLimit: The maximum total cost that the cache can hold
+    ///     before it starts evicting objects. If 0, there is no total cost limit. The
+    ///     default value is 0.
+    ///
     public init(expiry: Expiry = .never, countLimit: UInt = 0, totalCostLimit: UInt = 0) {
         self.expiry = expiry
         self.countLimit = countLimit
